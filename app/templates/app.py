248ccbaa8adf4d76ecf7c07edcee21ec424800d0
ducktheduck <66016734+ducktheduck@users.noreply.github.com>
2020-12-04 23:11:02 +0200

    Fixing some issues, testing the method.

--- app/templates/app.py
+++ app/templates/app.py
@@ -1,15 +1,23 @@
 from flask import Flask, session, redirect, url_for, request, make_response, render_template
 import pytube
+import os
 app = Flask(__name__,template_folder='Front-End')
-
 @app.route('/', methods = ['POST','GET'])
 def index(name = None):
 
     if request.method == "GET":
-        print(request.args)
+        print(request.form.get('input'))
         if request.args.get('input'):
-            print(request.args.get('input'))
-        print('ce fa ma')
+            video_url = request.args.get('input')
+            try:
+
+                youtube = pytube.YouTube(video_url)
+                video = youtube.streams.first()
+                video.download(f'{os.path.abspath(os.getcwd())}/output')
+                return render_template()
+
+            except Exception as e:
+                print(f"Error: {e}")
 
     return render_template('index.html', data = {"title":'duckcave'})
 

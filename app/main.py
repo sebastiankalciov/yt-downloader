33d910bb5f2cbf039dcfedbf07899f22536742d9
ducktheduck <66016734+ducktheduck@users.noreply.github.com>
2020-12-04 12:26:02 +0200

    Fixing the main code, updates lates.

---
+++ app/main.py
@@ -0,0 +1,15 @@
+import pytube
+video_url = input('Type the link of the youtube video: ')
+
+try:
+
+    youtube = pytube.YouTube(video_url)
+    video = youtube.streams.first()
+    video.download('./output')
+    print('Video downloaded successfully!')
+
+except Exception as e:
+    print(f'Error: {e}')
+
+import os
+os.system("pause")

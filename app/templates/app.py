8245a2ad25e2f479554f8f9eddb3a0d1343605f9
ducktheduck <66016734+ducktheduck@users.noreply.github.com>
2020-12-04 22:40:31 +0200

    Adding the form and method.

--- app/templates/app.py
+++ app/templates/app.py
@@ -1,9 +1,12 @@
 from flask import Flask, session, redirect, url_for, request, make_response, render_template
-
+import pytube
 app = Flask(__name__,template_folder='Front-End')
 
 @app.route('/', methods = ['POST','GET'])
 def index(name = None):
+
+    if request.method == "GET":
+        print('ce fa ma')
 
     return render_template('index.html', data = {"title":'duckcave'})
 

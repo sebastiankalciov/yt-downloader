49bcb858beb330ce5db8cd55a7cc736ad955ca45
ducktheduck <66016734+ducktheduck@users.noreply.github.com>
2020-12-04 23:25:32 +0200

    Fixing some syntax minor issues. Good to go.

--- app/templates/app.py
+++ app/templates/app.py
@@ -1,12 +1,13 @@
 from flask import Flask, session, redirect, url_for, request, make_response, render_template
 import pytube
 import os
+
 app = Flask(__name__,template_folder='Front-End')
+
 @app.route('/', methods = ['POST','GET'])
 def index(name = None):
 
     if request.method == "GET":
-        print(request.form.get('input'))
         if request.args.get('input'):
             video_url = request.args.get('input')
             try:
@@ -14,16 +15,14 @@ def index(name = None):
                 youtube = pytube.YouTube(video_url)
                 video = youtube.streams.first()
                 video.download(f'{os.path.abspath(os.getcwd())}/output')
-                return redirect(url_for('successPage'))
+
+                return render_template('success.html')
 
             except Exception as e:
+                return render_template('error.html')
                 print(f"Error: {e}")
 
     return render_template('index.html')
-
-@app.route('/success')
-def successPage():
-    return render_template('success.html')
 
 if __name__ == "__main__":
     app.run(debug=True)

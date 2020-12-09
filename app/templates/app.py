fbaf7ae964a9c33c980d486f2860158004aba9a6
ducktheduck <66016734+ducktheduck@users.noreply.github.com>
2020-12-09 20:51:59 +0200

    Code commented.

--- app/templates/app.py
+++ app/templates/app.py
@@ -2,27 +2,31 @@ from flask import Flask, session, redirect, url_for, request, make_response, ren
 import pytube
 import os
 
-app = Flask(__name__,template_folder='Front-End')
+app = Flask(__name__, template_folder='Front-End')
 
 @app.route('/', methods = ['POST','GET'])
 def index(name = None):
 
-    if request.method == "GET":
-        if request.args.get('input'):
-            video_url = request.args.get('input')
-            try:
+    if request.method == "GET": # Check if the request is GET method.
+
+        if request.args.get('input'): # Check if there s a link given.
+
+            video_url = request.args.get('input') # Variable that memories the link.
+            try:  
 
                 youtube = pytube.YouTube(video_url)
                 video = youtube.streams.first()
-                video.download(f'{os.path.abspath(os.getcwd())}/output')
+                video.download(f'{os.path.abspath(os.getcwd())}/output') # Downloads the video in ~~output~~ folder.
 
-                return render_template('finalPage.html', data = {"message":"YouTube video downloaded successfully!"})
+                return render_template('finalPage.html', data = {"message":"YouTube video downloaded successfully!"}) # Returns a message if the video was downloaded successfully.
 
             except Exception as e:
-                return render_template('finalPage.html', data = {"message":"Error: Issue related to download attribute, check if your link is alright."})
+
+                return render_template('finalPage.html', data = {"message":"Error: Issue related to download attribute, check if your link is alright."}) # Returns a message if there s any error.
                 print(f"Error: {e}")
 
-    return render_template('index.html')
+    return render_template('index.html') # Returning the index if there s no input value.
 
 if __name__ == "__main__":
+
     app.run(debug=True)

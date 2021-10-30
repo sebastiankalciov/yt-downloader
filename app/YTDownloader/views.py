ce5f0cfeae15ac85cfb8c551debd8f3e899feef9
Sebastian Kalciov <66016734+ducktheduck@users.noreply.github.com>
2021-10-30 23:04:06 +0300

    Finished some tests, works like a charm

--- app/YTDownloader/views.py
+++ app/YTDownloader/views.py
@@ -19,18 +19,17 @@ def downloadPage_view(request, *args, **kwargs):
     return render(request, "download.html", {})
 
 def successPage_view(request, *args, **kwargs):
-    response = "duten mata"
+    response = False
     if request.GET["input"]:
         video_url = request.GET["input"]
         try:  
-            print('ce fa ma')
-            print(pytube.YouTube('youtu.be/LVoF64LOgL0'))
-            #youtube = pytube.YouTube(video_url)
-            #video = youtube.streams.first()
-            #video.download(f'{os.path.abspath(os.getcwd())}/output')
 
+            youtube = pytube.YouTube(video_url)
+            video = youtube.streams.first()
+            video.download(f'{os.path.abspath(os.getcwd())}/output')
+            response = True
         except Exception as e:
-            response = "duten plm"
-            print(e)
+            response = False
+
 
     return render(request, "success.html", {"response":response})
